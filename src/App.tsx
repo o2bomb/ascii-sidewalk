d62a91a627ccee1686596def176abbdf43e6f4db
felix t <41817193+o2bomb@users.noreply.github.com>
2023-04-02 17:35:16 +0800

    Text

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ const App = () => {
     const frame = ctx.getImageData(0, 0, canvasEl.width, canvasEl.height);
     const frameData = frame.data;
     let text = "";
+    let textIndex = 0;
     for (let i = 0; i < frameData.length; i += 4) {
       let r = frameData[i];
       let g = frameData[i + 1];
@@ -48,10 +49,11 @@ const App = () => {
       // Then convert to either black or white depending on luminance value
       frameData[i] = frameData[i + 1] = frameData[i + 2] = l > 140 ? 255 : 0;
 
-      text += l > 140 ? "0" : "⠀";
+      text += l > 140 ? TEXT[textIndex] : "⠀";
       if ((i / 4) % frame.width === frame.width - 1) {
         text += "\n";
       }
+      textIndex = (textIndex + 1) % TEXT.length;
     }
     asciiEl.innerHTML = text;
 
@@ -94,3 +96,5 @@ const App = () => {
 };
 
 export default App;
+
+const TEXT = `Amidst⠀the⠀swirling⠀cosmos,⠀Amidst⠀the⠀stars⠀that⠀gleam,⠀Amidst⠀the⠀endless⠀voids⠀of⠀space,⠀A⠀precious⠀thing⠀doth⠀teem.⠀The⠀planet⠀Earth,⠀a⠀tiny⠀speck,⠀A⠀beacon⠀in⠀the⠀night,⠀And⠀upon⠀its⠀surface⠀doth⠀exist,⠀A⠀wondrous,⠀living⠀sight.⠀For⠀here⠀upon⠀this⠀little⠀world,⠀Amidst⠀the⠀oceans⠀blue,⠀The⠀mountains⠀high,⠀the⠀forests⠀green,⠀The⠀humans⠀do⠀pursue.⠀A⠀species⠀unlike⠀any⠀other,⠀With⠀curious,⠀seeking⠀minds,⠀With⠀hearts⠀that⠀beat⠀with⠀passion⠀strong,⠀And⠀souls⠀that⠀seek⠀to⠀find.⠀They've⠀conquered⠀lands,⠀they've⠀built⠀great⠀cities,⠀They've⠀flown⠀across⠀the⠀sky,⠀They've⠀charted⠀every⠀inch⠀of⠀space,⠀And⠀yet,⠀they⠀wonder⠀why.⠀For⠀deep⠀within⠀the⠀human⠀heart,⠀There⠀lies⠀a⠀restless⠀fire,⠀A⠀hunger⠀for⠀the⠀unknown,⠀A⠀thirst⠀for⠀something⠀higher.⠀They⠀strive⠀for⠀love,⠀they⠀strive⠀for⠀truth,⠀They⠀yearn⠀for⠀understanding,⠀They⠀seek⠀to⠀touch⠀the⠀face⠀of⠀God,⠀To⠀reach⠀for⠀something⠀grand.⠀And⠀though⠀they⠀oftentimes⠀fall⠀short,⠀And⠀make⠀mistakes⠀along⠀the⠀way,⠀The⠀humans⠀are⠀a⠀remarkable⠀breed,⠀That⠀grow⠀and⠀learn⠀each⠀day.⠀So⠀let⠀us⠀honor⠀all⠀these⠀humans,⠀With⠀their⠀laughter⠀and⠀their⠀tears,⠀For⠀they⠀are⠀a⠀precious⠀gift,⠀That⠀brightens⠀up⠀the⠀years.`;
